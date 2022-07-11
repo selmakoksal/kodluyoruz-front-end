a22afd1399d5ce2ee4faf728da83e3a56ebdf32c
Selma Köksal <98459047+selmakoksal@users.noreply.github.com>
2022-07-11 14:00:07 +0300

    delete odev-1

--- js-odevleri/odev-1/js/clock.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("myName").innerHTML = prompt("Adınız nedir?")
-function showTime() {
-    let date = new Date();
-    let days = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
-    let clock = document.getElementById('myClock')
-    clock.innerHTML = `${('0' + date.getHours()).slice(-2)}:${('0' + date.getMinutes()).slice(-2)}:${('0' + date.getSeconds()).slice(-2)} ${days[date.getUTCDay()]}`
-    setTimeout(showTime, 1)
-}
-showTime();
